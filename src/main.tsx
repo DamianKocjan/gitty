86b27dfb437f7d35d9183c7045dd8d8e1fac4864
Damian Kocjan <DamianoKPL@wp.pl>
2024-09-24 00:43:35 +0200

    add shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
+import { App } from "./app";
+import { ThemeProvider } from "./components/theme-provider";
+
+import "./globals.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
