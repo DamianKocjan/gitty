bc0ceec28355c2e0c703fbbcaecbcb06ee2726a0
Damian Kocjan <DamianoKPL@wp.pl>
2024-10-29 23:41:41 +0100

    refactor: spawn shell and then run git

--- src-tauri/src/utils/subprocess.rs
+++ src-tauri/src/utils/subprocess.rs
@@ -1,3 +1,17 @@
+#[cfg(target_os = "windows")]
+const SHELL: &str = "cmd";
+
+#[cfg(not(target_os = "windows"))]
+const SHELL: &str = "sh";
+
+#[cfg(target_os = "windows")]
+const SHELL_FLAG: &str = "/C";
+
+#[cfg(not(target_os = "windows"))]
+const SHELL_FLAG: &str = "-c";
+
 pub fn create_git_cli() -> std::process::Command {
-    std::process::Command::new("git")
+    let mut shell = std::process::Command::new(SHELL);
+    shell.arg(SHELL_FLAG).arg("git");
+    shell
 }
